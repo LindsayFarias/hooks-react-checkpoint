4b58bc5e0cd22e8b96f842e67512b046c42579b7
Lindsay Farias <lindsayfarias@gmail.com>
2021-10-13 16:33:41 -0400

    Everything is broken, not a fan of hooks

---
+++ products-page/src/ProductCard.js
@@ -0,0 +1,24 @@
+import {CardGroup, Card} from "react-bootstrap";
+
+const ProductCard = ({products, dataFunc, data}) => {
+    
+    let productCards = products[0].map((product) => {
+        return(
+            <Card>
+                <Card.Body>
+                    <Card.Title>Product: {product.name}</Card.Title>
+                    <Card.Text>{product.slogan}</Card.Text>
+                </Card.Body>
+                <Card.Footer onClick={()=>{dataFunc(product.id)}}> <small className="text-muted">See Details</small> </Card.Footer>
+            </Card>
+        )
+    })
+
+    return(
+        <CardGroup>
+            {productCards}
+        </CardGroup>
+    )
+}
+
+export default ProductCard;
